90ec81de090aae527c29fa5e1fefdf465c9c780c
Patrick <patrickjyoung97@gmail.com>
2019-10-15 21:33:10 +1100

    Added 2 more graphs

--- googlescholarDataRep.py
+++ googlescholarDataRep.py
@@ -15,6 +15,31 @@ import time
 def howManyHyphens(title):
     return title.count('-');
 
+def placeinLengthList(titleLength,titleLengthList,hyphenCount,lengthTotalAmountofPapers,citeCount):
+    if titleLength > 150:
+        titleLengthList[hyphenCount][6] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][6]+= 1
+    elif titleLength > 125:
+        titleLengthList[hyphenCount][5] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][5]+= 1
+    elif titleLength > 100:
+        titleLengthList[hyphenCount][4] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][4]+= 1
+    elif titleLength > 75:
+        titleLengthList[hyphenCount][3] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][3]+= 1
+    elif titleLength > 50:
+        titleLengthList[hyphenCount][2] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][2]+= 1
+    elif titleLength > 25:
+        titleLengthList[hyphenCount][1] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][1]+= 1
+    else:
+        titleLengthList[hyphenCount][0] += citeCount
+        lengthTotalAmountofPapers[hyphenCount][0]+= 1
+
+
+
 
 def main():
     
@@ -32,6 +57,18 @@ def main():
     hyphenFive = []
     filelist = []
     prevfilelist = []
+    totaltitleLengthList = [0,0,0,0,0,0]
+    meanTitleLengthList = [0,0,0,0,0,0]
+
+    ## Hyphen Title counts for the ranges 0-25, 25-50 , 50-75, 75-100, 100-125, 125-150, 150-max
+    f5List0 = [0,0,0,0,0,0,0]
+    f5List1 = [0,0,0,0,0,0,0]
+    f5List2 = [0,0,0,0,0,0,0]
+    f5List3 = [0,0,0,0,0,0,0]
+    f5List4 = [0,0,0,0,0,0,0]
+    f5List5 = [0,0,0,0,0,0,0]
+    hyphenCountTitleLengthCiteCount = [f5List0,f5List1,f5List2,f5List3,f5List4,f5List5]
+    lengthTotalAmountofPapers = [f5List0.copy(),f5List1.copy(),f5List2.copy(),f5List3.copy(),f5List4.copy(),f5List5.copy()]
     
     
 
@@ -67,196 +104,263 @@ def main():
     plt.title('Mean Citation Count and Hyphens')
 
 
-    #Needed info for figure 3
+   
+
+
+
+    
+    articleUpdated = [False,False,False,False,False,False]
+    time.sleep(10)
+
+    
+    for file in glob.glob("Set 3/*.txt"):
+        if file not in filelist:
+            filelist.append(file)
+            
+    
+
+    print(prevfilelist)
+    newfilelist = list(set(filelist) - set(prevfilelist))
+    
+    for fileObj in newfilelist:
+        
+        file = fileObj
+        dictobjects = []
+        dic_list_op = open(file,'r',encoding = "utf8")
+        dic_list_str = dic_list_op.read()
+        dic_list_op.close()
+
+        dic_list_split_arr = dic_list_str.split('}{')
+
+        for index,x in zip(range(len(dic_list_split_arr)),dic_list_split_arr): 
+            if index != 0 and index != (len(dic_list_split_arr)- 1) :
+                dictobjects.append(ast.literal_eval("{" + x + "}"))
+            elif index == (len(dic_list_split_arr)- 1) :
+                dictobjects.append(ast.literal_eval("{" + x))
+            else:
+                y = x.split('{',1)[1]
+                dictobjects.append(ast.literal_eval("{" + y + "}"))
+        
+                
+        
+
+
+        for index,x in zip(range(len(dictobjects)),dictobjects):
+            
+            if "citedby" in x:
+                if "bib" in x and x["citedby"] > 100:
+                    if "title" in x["bib"]:
+                        hyphenCount = howManyHyphens(x["bib"]["title"])
+                        titleLength = len(x["bib"]["title"])
+                        if(hyphenCount == 0):
+                            
+                            citationCount[0] += x["citedby"]
+                            articleCount[0] += 1
+                            hyphenNull.append(x["citedby"])
+                            articleUpdated[0] = True
+                            totaltitleLengthList[0] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,0,lengthTotalAmountofPapers,x["citedby"])
+                            
+                            
+                        elif(hyphenCount == 1):
+                            citationCount[1] += x["citedby"]
+                            articleCount[1] += 1
+                            hyphenOne.append(x["citedby"])
+                            articleUpdated[1] = True
+                            totaltitleLengthList[1] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,1,lengthTotalAmountofPapers,x["citedby"])
+                            
+                        elif(hyphenCount == 2):
+                            citationCount[2] += x["citedby"]
+                            articleCount[2] += 1
+                            hyphenTwo.append(x["citedby"])
+                            articleUpdated[2] = True
+                            totaltitleLengthList[2] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,2,lengthTotalAmountofPapers,x["citedby"])
+                            
+                        elif(hyphenCount == 3):
+                            citationCount[3] += x["citedby"]
+                            articleCount[3] += 1
+                            hyphenThree.append(x["citedby"])
+                            articleUpdated[3] = True
+                            totaltitleLengthList[3] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,3,lengthTotalAmountofPapers,x["citedby"])
+                            
+                        elif(hyphenCount == 4):
+                            citationCount[4] += x["citedby"]
+                            articleCount[4] += 1
+                            articleUpdated[4] = True
+                            hyphenFour.append(x["citedby"])
+                            totaltitleLengthList[4] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,4,lengthTotalAmountofPapers,x["citedby"])
+                            
+                        elif(hyphenCount >= 5):
+                            citationCount[5] += x["citedby"]
+                            articleCount[5] += 1
+                            hyphenFive.append(x["citedby"])
+                            articleUpdated[5] = True
+                            totaltitleLengthList[5] += titleLength
+                            placeinLengthList(titleLength,hyphenCountTitleLengthCiteCount,5,lengthTotalAmountofPapers,x["citedby"])
+
+
+
+
+    
+    print(*citationCount)
+    for num,citeCount in enumerate(citationCount,start=0):
+        if(articleCount[num] != 0) and (len(newfilelist) != 0) and articleUpdated[num]:
+            meanCitationCount[num] = citeCount/articleCount[num]
+
+    print(*meanCitationCount)
+    
+    
+
+    
+
+    totalcount = 0
+    for x in articleCount:
+        totalcount += x
+    
+    if totalcount == 0:
+        totalcount = 1
+    
+    
+    
+##    data = [[articleCount[0],round(articleCount[0]/totalcount)],
+##                        [articleCount[1],round(articleCount[1]/totalcount)],[articleCount[2],round(articleCount[2]/totalcount)],
+##                         [articleCount[3],round(articleCount[3]/totalcount)],[articleCount[4],round(articleCount[4]/totalcount)],
+##                         [articleCount[5],round(articleCount[5]/totalcount)],[totalcount,100]
+##                         ]
+    data = [[articleCount[0],articleCount[1],articleCount[2],articleCount[3],articleCount[4],articleCount[5],totalcount],
+            [round((articleCount[0]/totalcount)*100,2),round((articleCount[1]/totalcount)*100,2) , round((articleCount[2]/totalcount)* 100,2) ,
+             round((articleCount[3]/totalcount) * 100,2)
+             ,round((articleCount[4]/totalcount) * 100,2),
+             round((articleCount[5]/totalcount) * 100,2),100]]
+    
+
+   
+
+   
+    plt.figure(1)
+    the_table = plt.table(cellText=data,
+                      rowLabels=rows,
+                      rowColours=colors,
+                      colLabels=columns,
+                          loc = "center")
+    plt.subplots_adjust(left=0.2,top = 0.8)
+    
+    f1.canvas.draw()
+    f1.canvas.flush_events()
+    plt.pause(0.05)
+    
+    
+
+
+    plt.figure(2)
+    f2.clear()
+    plt.bar(y_pos,meanCitationCount,align = 'center',color = (0.2, 0.4, 0.6, 0.6))
+    plt.bar(y_pos,meanCitationCount,align = 'center', alpha = 0.5)
+    plt.xticks(y_pos,objects)
+    plt.ylabel('Mean Citation Count')
+    plt.title('Mean Citation Count and Hyphens')
+
+    
+    f2.canvas.draw()
+    plt.pause(0.05)
+    
+    
+
     HyphenLabel = []
-    countData = 0
+
+    for x in hyphenNull:
+        HyphenLabel.append('0')
+
+    for x in hyphenOne:
+        HyphenLabel.append('1')
+
+    for x in hyphenTwo:
+        HyphenLabel.append('2')
+
+    for x in hyphenThree:
+        HyphenLabel.append('3')
+
+    for x in hyphenFour:
+        HyphenLabel.append('4')
+
+    for x in hyphenFive:
+        HyphenLabel.append('5')
+
+    countData = hyphenNull + hyphenOne + hyphenTwo + hyphenThree + hyphenFour + hyphenFive
+
     boxData = {'Hyphens':HyphenLabel,'Count':countData}
-    df = pd.DataFrame()
-    boxplot = df.boxplot()
-    f3 = plt.figure(3)
+    
+    df = pd.DataFrame(boxData)
+    
+    print(df)
+    
+   
+    
+
+    
+    boxplot = df.boxplot(column ='Count', by ='Hyphens')
+    
+    
+    
+    plt.pause(0.05)
+    f4 = plt.figure(4)
+    for num,titlelength in enumerate(totaltitleLengthList,start=0):
+        if(articleCount[num] != 0) and (len(newfilelist) != 0) and articleUpdated[num]:
+            meanTitleLengthList[num] = titlelength/articleCount[num]
+
+    plt.bar(y_pos,meanTitleLengthList,align = 'center',color = (0.2, 0.4, 0.6, 0.6))
+    plt.bar(y_pos,meanTitleLengthList,align = 'center', alpha = 0.5)
+    plt.xticks(y_pos,objects)
+    plt.ylabel('Mean title length')
+    plt.title('Mean Title length related to hyphen count')
+
+    
+    f4.canvas.draw()
+    plt.pause(0.05)
+
+    #f5,ax3 = plt.subplots()
+##    f5w = 0.3
+
+    f5x = ['0','1','2','3','4','>4']
+    valListList = [[],[],[],[],[],[],[]]
+
+    for i in range(7):
+        for j in range(6):
+            if lengthTotalAmountofPapers[j][i] == 0:
+                number = 0
+            else:
+                number = hyphenCountTitleLengthCiteCount[j][i]/lengthTotalAmountofPapers[j][i]
+            valListList[i].append(number)
+    
+    #valListList[0] = [hyphenCountTitleLengthCiteCount[0][0]/lengthTotalAmountofPapers[0][0]]
 
 
+    
 
-    #Main Loop
-    while 1:
-        articleUpdated = [False,False,False,False,False,False]
-        time.sleep(10)
-
-        
-        for file in glob.glob("Set 3/*.txt"):
-            if file not in filelist:
-                filelist.append(file)
-                
-        
-
-        print(prevfilelist)
-        newfilelist = list(set(filelist) - set(prevfilelist))
-        
-        for fileObj in newfilelist:
-            
-            file = fileObj
-            dictobjects = []
-            dic_list_op = open(file,'r',encoding = "utf8")
-            dic_list_str = dic_list_op.read()
-            dic_list_op.close()
-
-            dic_list_split_arr = dic_list_str.split('}{')
-
-            for index,x in zip(range(len(dic_list_split_arr)),dic_list_split_arr): 
-                if index != 0 and index != (len(dic_list_split_arr)- 1) :
-                    dictobjects.append(ast.literal_eval("{" + x + "}"))
-                elif index == (len(dic_list_split_arr)- 1) :
-                    dictobjects.append(ast.literal_eval("{" + x))
-                else:
-                    y = x.split('{',1)[1]
-                    dictobjects.append(ast.literal_eval("{" + y + "}"))
-            
-                    
-            
-
-
-            for index,x in zip(range(len(dictobjects)),dictobjects):
-                
-                if "citedby" in x:
-                    if "bib" in x and x["citedby"] > 100:
-                        if "title" in x["bib"]:
-                            hyphenCount = howManyHyphens(x["bib"]["title"])
-                            if(hyphenCount == 0):
-                                citationCount[0] += x["citedby"]
-                                articleCount[0] += 1
-                                hyphenNull.append(x["citedby"])
-                                articleUpdated[0] = True
-                            elif(hyphenCount == 1):
-                                citationCount[1] += x["citedby"]
-                                articleCount[1] += 1
-                                hyphenOne.append(x["citedby"])
-                                articleUpdated[1] = True
-                            elif(hyphenCount == 2):
-                                citationCount[2] += x["citedby"]
-                                articleCount[2] += 1
-                                hyphenTwo.append(x["citedby"])
-                                articleUpdated[2] = True
-                            elif(hyphenCount == 3):
-                                citationCount[3] += x["citedby"]
-                                articleCount[3] += 1
-                                hyphenThree.append(x["citedby"])
-                                articleUpdated[3] = True
-                            elif(hyphenCount == 4):
-                                citationCount[4] += x["citedby"]
-                                articleCount[4] += 1
-                                articleUpdated[4] = True
-                                hyphenFour.append(x["citedby"])
-                            elif(hyphenCount >= 5):
-                                citationCount[5] += x["citedby"]
-                                articleCount[5] += 1
-                                hyphenFive.append(x["citedby"])
-                                articleUpdated[5] = True
-
-
-
-
-        
-        print(*citationCount)
-        for num,citeCount in enumerate(citationCount,start=0):
-            if(articleCount[num] != 0) and (len(newfilelist) != 0) and articleUpdated[num]:
-                meanCitationCount[num] = citeCount/articleCount[num]
-
-        print(*meanCitationCount)
-        
-        
-
-        
-
-        totalcount = 0
-        for x in articleCount:
-            totalcount += x
-        
-        if totalcount == 0:
-            totalcount = 1
-        
-        
-        
-    ##    data = [[articleCount[0],round(articleCount[0]/totalcount)],
-    ##                        [articleCount[1],round(articleCount[1]/totalcount)],[articleCount[2],round(articleCount[2]/totalcount)],
-    ##                         [articleCount[3],round(articleCount[3]/totalcount)],[articleCount[4],round(articleCount[4]/totalcount)],
-    ##                         [articleCount[5],round(articleCount[5]/totalcount)],[totalcount,100]
-    ##                         ]
-        data = [[articleCount[0],articleCount[1],articleCount[2],articleCount[3],articleCount[4],articleCount[5],totalcount],
-                [round((articleCount[0]/totalcount)*100,2),round((articleCount[1]/totalcount)*100,2) , round((articleCount[2]/totalcount)* 100,2) ,
-                 round((articleCount[3]/totalcount) * 100,2)
-                 ,round((articleCount[4]/totalcount) * 100,2),
-                 round((articleCount[5]/totalcount) * 100,2),100]]
-        
-
-       
-
-       
-        plt.figure(1)
-        the_table = plt.table(cellText=data,
-                          rowLabels=rows,
-                          rowColours=colors,
-                          colLabels=columns,
-                              loc = "center")
-        plt.subplots_adjust(left=0.2,top = 0.8)
-        
-        f1.canvas.draw()
-        f1.canvas.flush_events()
-        plt.pause(0.05)
-        
-        
-
-
-        plt.figure(2)
-        f2.clear()
-        plt.bar(y_pos,meanCitationCount,align = 'center',color = (0.2, 0.4, 0.6, 0.6))
-        plt.bar(y_pos,meanCitationCount,align = 'center', alpha = 0.5)
-        plt.xticks(y_pos,objects)
-        plt.ylabel('Mean Citation Count')
-        plt.title('Mean Citation Count and Hyphens')
+    f5Data = {'0-25': valListList[0], '25-50': valListList[1], '50-75': valListList[2], '75-100': valListList[3],
+              '100-125': valListList[4],'125-150': valListList[5],'150-max': valListList[6] }
+    df5 =  pd.DataFrame(f5Data,index = f5x)
+    axf5 = df5.plot.bar(rot = 0)
+    print(df5)
     
         
-        f2.canvas.draw()
-        plt.pause(0.05)
-        plt.figure(3)
-        f3.clear()
-        boxplot.clear()
+##    ax3.bar(f5x-0.6,valListList[0],width = 0.2,align = 'center')
+##    ax3.bar(f5x-0.4,valListList[1],width = 0.2,align = 'center')
+##    ax3.bar(f5x-0.2,valListList[2],width = 0.2,align = 'center')
+##    ax3.bar(f5x,valListList[3],width = 0.2,align = 'center')
+##    ax3.bar(f5x+0.2,valListList[4],width = 0.2,align = 'center')
+##    ax3.bar(f5x+0.4,valListList[5],width = 0.2,align = 'center')
+    plt.show()
 
-        HyphenLabel = []
 
-        for x in hyphenNull:
-            HyphenLabel.append('0')
-
-        for x in hyphenOne:
-            HyphenLabel.append('1')
-
-        for x in hyphenTwo:
-            HyphenLabel.append('2')
-
-        for x in hyphenThree:
-            HyphenLabel.append('3')
-
-        for x in hyphenFour:
-            HyphenLabel.append('4')
-
-        for x in hyphenFive:
-            HyphenLabel.append('5')
-
-        countData = hyphenNull + hyphenOne + hyphenTwo + hyphenThree + hyphenFour + hyphenFive
-
-        boxData = {'Hyphens':HyphenLabel,'Count':countData}
-        
-        df = pd.DataFrame(boxData)
-        
-        print(df)
-        
-
-        
-        boxplot = df.boxplot(column ='Count', by ='Hyphens')
-        print(boxplot.get_figure())
-        
-        plt.pause(0.05)
-        
-        prevfilelist = filelist.copy()
+    
+    
+    
+    prevfilelist = filelist.copy()
 
         
 
